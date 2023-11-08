e7b58e8e776729f43b4809c1015f5f10820b5a5c
Matayush <ceniuk.mateusz@gmail.com>
2023-11-08 10:54:32 +0100

    file changes

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -2,7 +2,11 @@ package org.example;
 
 public class Hello {
     public static void main(String[] args) {
+<<<<<<< HEAD
         String example = "Hello world";
         System.out.println(example);
+=======
+        System.out.println("Hello world!");
+>>>>>>> 07a2aee (file changes)
     }
 }
